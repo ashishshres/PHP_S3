f69d1d14be54967810db36f302a6dabc1d1f79d9
Ashish Shrestha <ashishshr27@gmail.com>
2026-01-22 15:08:52 +0545

    chore: include php in page

--- 01_basics/index.php
+++ 01_basics/index.php
@@ -8,6 +8,52 @@
 </head>
 
 <body>
+    <section>
+        <!-- 1. Insert PHP code to print your faculty using print() -->
+        <?php
+        print("Education");
+        ?>
+    </section>
+
+    <section>
+        <!-- 2. Insert PHP code to declare a variable named `fullname` and print its value using echo() -->
+        <?php
+        $fullname = "Ashish Shrestha";
+        echo ("My name is $fullname");
+        ?>
+    </section>
+
+    <section>
+        <!-- 3. Insert PHP code to print your fullname without any function -->
+        <?=
+        "Ashish Shrestha";
+        ?>
+    </section>
+
+    <section>
+        <!-- 4. Insert PHP code to print your favorite fruit inside heading -->
+        <h1>
+            <?php
+            print("Apple");
+            ?>
+        </h1>
+    </section>
+
+    <section>
+        <!-- 5. Insert PHP code to declare a constant value `birthYear` and print its value -->
+        <?php
+        define("birthYear", "2060 B.S.");
+        print(birthYear);
+        ?>
+    </section>
+
+    <section>
+        <!-- 6. Insert PHP code to insert a `h2` and `button` -->
+        <?php
+        print("<h2>Heading 2</h2>");
+        print("<button>Click me</button>");
+        ?>
+    </section>
 
 </body>
 
